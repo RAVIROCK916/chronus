7ea13c199c48f3cbbbd88bab54db0662db853cfc
Ravi Teja Pedapudi <prt15@iitbbs.ac.in>
2025-01-30 19:25:12 +0530

    forgot to add apollo client file

---
+++ src/lib/apollo/index.ts
@@ -0,0 +1,18 @@
+import { HttpLink } from "@apollo/client";
+import {
+  ApolloClient,
+  InMemoryCache,
+} from "@apollo/experimental-nextjs-app-support";
+import { SERVER_URL } from "@/constants";
+
+export default function makeClient() {
+  const httpLink = new HttpLink({
+    uri: SERVER_URL,
+    fetchOptions: { cache: "no-store" },
+  });
+
+  return new ApolloClient({
+    cache: new InMemoryCache(),
+    link: httpLink,
+  });
+}
