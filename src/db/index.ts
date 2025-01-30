4c699b72e2bf30e289c3aacbc67bcb87ba4f445f
Ravi Teja Pedapudi <prt15@iitbbs.ac.in>
2025-01-30 19:24:46 +0530

    changes to db schema

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,6 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
+import * as schema from "./schema";
 
 const connectionString = process.env.DATABASE_URL;
 
@@ -7,6 +8,6 @@ const pool = new Pool({
   connectionString,
 });
 
-const db = drizzle({ client: pool });
+const db = drizzle({ client: pool, schema });
 
 export default db;
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,16 +1,18 @@
 import { pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
 
 export const userTable = pgTable("users", {
-  id: uuid("id").primaryKey(),
+  id: uuid("id").primaryKey().defaultRandom(),
   name: text("name").notNull(),
-  email: text("email").notNull(),
+  email: text("email").unique().notNull(),
   password_hash: text("password_hash").notNull(),
   created_at: timestamp("created_at").defaultNow().notNull(),
 });
 
 export const sessionTable = pgTable("sessions", {
-  id: uuid("id").primaryKey(),
-  user_id: uuid("user_id").notNull(),
+  id: uuid("id").primaryKey().defaultRandom(),
+  user_id: uuid("user_id")
+    .references(() => userTable.id)
+    .notNull(),
   expires_at: timestamp("expires_at").notNull(),
   created_at: timestamp("created_at").defaultNow().notNull(),
 });
