97d300dca10bc2c9dc1ff064806799ad44a4507b
Ravi Teja Pedapudi <prt15@iitbbs.ac.in>
2025-02-16 18:16:59 +0530

    feat: add middleware to the apollo server

--- src/app/api/graphql/route.ts
+++ src/app/api/graphql/route.ts
@@ -3,11 +3,23 @@ import { startServerAndCreateNextHandler } from "@as-integrations/next";
 
 import { typeDefs } from "./schema/type-defs";
 import { resolvers } from "./schema/resolvers";
+import { NextRequest } from "next/server";
+import { cookies } from "next/headers";
+import { decryptSession } from "@/lib/session";
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
-const handler = startServerAndCreateNextHandler(server);
+const handler = startServerAndCreateNextHandler(server, {
+  context: async (req: NextRequest) => {
+    const token = cookies().get("sessionId")?.value || "";
+    // if there is no token, return an empty object
+    if (!token) return {};
 
-console.log("apollo server started", handler);
+    const decryptedToken = await decryptSession(token);
+    return { userId: decryptedToken?.userId };
+  },
+});
+
+console.log("apollo server started");
 
 export { handler as GET, handler as POST };
