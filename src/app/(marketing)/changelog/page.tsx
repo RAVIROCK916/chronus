f0ce3ed26e3641898cf67bde70b6cf62eb05afd5
Ravi Teja Pedapudi <prt15@iitbbs.ac.in>
2025-06-15 13:23:39 +0530

    client: add landing page components

--- src/app/(marketing)/changelog/page.tsx
+++ src/app/(marketing)/changelog/page.tsx
@@ -1,19 +1,19 @@
-import AnimationContainer from "@/components/global/animation-container";
-import React from 'react'
+import AnimationContainer from "@/components/main/animation-container";
+import React from "react";
 
 const ChangeLogPage = () => {
-    return (
-        <div className="flex flex-col items-center justify-center py-20">
-            <AnimationContainer delay={0.1}>
-                <h1 className="text-2xl md:text-4xl lg:text-5xl font-semibold font-heading text-center mt-6 !leading-tight">
-                    Change Log
-                </h1>
-                <p className="text-base md:text-lg mt-6 text-center text-muted-foreground">
-                    Stay up to date with the latest changes to our platform.
-                </p>
-            </AnimationContainer>
-        </div>
-    )
+  return (
+    <div className="flex flex-col items-center justify-center py-20">
+      <AnimationContainer delay={0.1}>
+        <h1 className="font-heading mt-6 text-center text-2xl font-semibold !leading-tight md:text-4xl lg:text-5xl">
+          Change Log
+        </h1>
+        <p className="mt-6 text-center text-base text-muted-foreground md:text-lg">
+          Stay up to date with the latest changes to our platform.
+        </p>
+      </AnimationContainer>
+    </div>
+  );
 };
 
-export default ChangeLogPage
+export default ChangeLogPage;
